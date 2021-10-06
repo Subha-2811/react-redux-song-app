03501365dfe0ce1ea01b4efb3894ebe9d62eb940
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2021-10-06 11:12:43 +0530

    Shows the detail of song selected

---
+++ src/components/SongDetail.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class SongDetail extends React.Component {
+  render() {
+    if (!this.props.selectedSong) {
+      return <div>Select a Song</div>;
+    }
+    return (
+      <div>
+        <h3>Detail for:</h3>
+        <p>
+          Title: {this.props.selectedSong.title} <br />
+          Duration: {this.props.selectedSong.duration}
+        </p>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { selectedSong: state.selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail);
